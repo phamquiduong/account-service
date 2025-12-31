a99a1071fd1e008723bcc359961750160d470399
phamquiduong <phamquiduong@outlook.com>
2025-12-31 16:06:57 +0900

    Add Swagger UI urls

--- src/main/urls.py
+++ src/main/urls.py
@@ -3,6 +3,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include("common.urls")),
     path("auth/", include("authentication.urls")),
     path("account/", include("account.urls")),
 ]
